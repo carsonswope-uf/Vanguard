6cfb969d8e7b743fd88393bf8b40600d8672992e
carsonswope-uf <54452980+carsonswope-uf@users.noreply.github.com>
2020-02-16 17:30:13 -0500

    Progress on scroll-down about page
    Also resized some texts using vm for active window changing

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -7,11 +7,17 @@ class Landing extends React.Component {
         return (
             <div className="container">
                 <h1 className="title">Welcome to Sunburst Yacht Charters</h1>
-                <div className="button">
-                    <Link to="/Booking" ><button className="buttontext">Book your stay today</button></Link>
-                </div>
+                    <div className="button">
+                        <Link to="/Booking" ><button className="buttontext">Make your reservation</button></Link>
+                    </div>
                 <img src={require('./back5.png')} alt='landing' className='responsive'/>
-                
+                <div className="about">About Us</div>
+                    <div className="aboutcontent">
+                        Docked off downtown Tampa’s upscale Harbour Island, Sunburst Yacht Charters’ 82-foot-vessel ‘Vanguard’ is the luxury yacht of choice for Tampa Bay’s elite. Our high-end luxury yacht rental offers guests the opportunity to customize one-of-a-kind excursions. We provide you access to unparalleled amenities, activities, and entertainment that is certain to make your event an extraordinary one that you will never forget.
+                    </div>
+                <img className="aboutimage" src={require('../assets/dock1.jpg')}/>
+                <img className="aboutimage1" src={require('../assets/dock1.jpg')}/>
+                <div className="aboutcontent1">To be filled</div>
             </div>
         )
     };
