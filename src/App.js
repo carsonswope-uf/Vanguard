f0fcf5d016c58ce0589a5c9b7042e890a77ffed8
carsonswope-uf <54452980+carsonswope-uf@users.noreply.github.com>
2020-04-14 21:09:23 -0400

    Deployed App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Landing from "./components/Landing/Landing";
 import Contact from "./components/Contact/Contact";
 import Booking from "./components/Booking/Booking";
 import NotFound from "./components/NotFound";
-import {BrowserRouter as Router, Switch, Route, Link, useParams} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 
 export default function App() {
@@ -30,10 +30,10 @@ export default function App() {
         <Route path="/Booking">
           <Booking/>
         </Route>
-        <Route path="/">
+        <Route path="/" exact>
           <Landing/>
         </Route>
-        <Route component={NotFound}/>
+        <Route path="*" component={NotFound}/>
       </Switch>
       <Footer/>
     </Router>
