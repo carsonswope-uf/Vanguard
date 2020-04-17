11ed8df4368aebde09107412fbad0266d940f8fd
carsonswope-uf <54452980+carsonswope-uf@users.noreply.github.com>
2020-04-17 01:38:10 -0400

    Deployed header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import vanlogo from './vanguard.png';
+import vanlogo from './sunburstlogo.png';
 import './header.css';
 
 //arrow makes us not need the return after props
 const Header = props => (
     <header className="toolbar">
         <nav className="toolbar-navigation">
-{/*the logo link contains a bug where the anchor tag lies slightly beneath the logo itself*/}
             <div><Link to="/"><img src={vanlogo} className="toolbar-logo" alt=""/></Link></div>
             <div className="push-right"/>
                 <div className="toolbar-nav-items">
                     <ul>
                         <li><Link to="/Gallery" >Gallery</Link></li>
+                        <li><Link to="/Booking" >Make a Reservation</Link></li>
                         <li><Link to="/Contact" >Contact Us</Link></li>
                     </ul>
                 </div>
